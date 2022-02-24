752af27b8fa04362809ee592fdcd7a8cb75222ff
George <gchriscarter@gmail.com>
2022-02-24 18:22:12 -0500

    setup new title view and render form in create action

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -7,6 +7,13 @@ class TitlesController < ApplicationController
     @title = Title.find_by(id: params[:id])
   end
 
+  def new
+  end
+
+  def create
+    render plain: params[:title]
+  end
+
   def update
   end
 
