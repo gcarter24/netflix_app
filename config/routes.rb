41f2f897916fc4a19323b8c3bd2e9fe5d4d29b00
George <gchriscarter@gmail.com>
2022-03-11 18:07:58 -0500

    update route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :titles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :titles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
