2fdda7a71cf5da2002a6a61d0b171c24f18d5194
George <gchriscarter@gmail.com>
2022-03-11 16:59:48 -0500

    add validations

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,2 +1,7 @@
 class Title < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :release_year, presence: true
+  validates :rating, presence: true
+  validates :media_type, presence: true
 end
