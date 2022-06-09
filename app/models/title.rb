c25573ce239067e0f4d2569e55a8e77e09a49cba
Chris Carter <gchriscarter@gmail.com>
2022-06-09 16:55:57 -0400

    add scopes for filtering

--- app/models/title.rb
+++ app/models/title.rb
@@ -4,4 +4,13 @@ class Title < ApplicationRecord
   validates :release_year, presence: true
   validates :rating, presence: true
   validates :media_type, presence: true
+
+  scope :filter_by_name, -> (name) { where('name like ?', "%#{name}%")}
+  scope :filter_by_director, -> (director) { where('director like ?', "%#{director}%")}
+  scope :filter_by_cast, -> (cast) { where('cast like ?', "%#{cast}%")}
+  scope :filter_by_rating, -> (rating) { where('rating like ?', "%#{rating}%")}
+  scope :filter_by_year, -> (year) { where('year like ?', "%#{year}%")}
+  scope :filter_by_duration, -> (duration) { where('duration like ?', "%#{duration}%")}
+  scope :filter_by_country, -> (country) { where('country like ?', "%#{country}%")}
+  
 end
